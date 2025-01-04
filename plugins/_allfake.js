82cfce13f3daa1620cda104e91b73a6bdd201b96
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-04 14:29:23 -0500

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -16,7 +16,7 @@ let b = a[Math.floor(Math.random() * a.length)]
 let sap = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann']
 let sgh = md
 let sgc = nnn
-let gata = 'https://i.imgur.com/EXTbyyn.jpg'
+let gata = 'https://qu.ax/eOTEH.jpeg'
 //let logo = 'https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg'
 let pp = await conn.profilePictureUrl(who).catch(_ => gata)
 //pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
